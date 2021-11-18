8e7d72eafc83d25165e75e2ab51afd3c2b86fba2
OM SONAWANE <80897667+om-sonawane@users.noreply.github.com>
2021-11-18 00:30:12 -0800

    commit.changes

--- drink_water_notification_using.py
+++ drink_water_notification_using.py
@@ -4,7 +4,6 @@ from plyer import notification
 if __name__ == "__main__":
     notification.notify(
         title="please drink water",
-        message="The human body comprises around 60% water It’s commonly recommended that 
-        you drink eight 8-ounce (237-mL) glasses of water per day (the 8×8 rule).
-        Although there’s little science behind this specific rule, staying hydrated is important.
-        Here are 7 evidence-based health benefits of drinking plenty of water"
+        message="The human body comprises around 60% water It’s commonly recommended that ",
+        app_icon="C:\Users\admin\Desktop\drink-water-notifiction ",
+        timeout=5
